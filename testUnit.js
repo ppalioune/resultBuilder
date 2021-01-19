34562f580cd3cf1c385701bc8face657b614ffee
ppalioune <ppalioune@gmail.com>
2021-01-19 16:33:18 -0500

    updatefile test Unit

--- testUnit.js
+++ testUnit.js
@@ -2,37 +2,39 @@ const ResultBuilder = require('./resultBuilder')
 const Test = require('./test')
 const assert = require('assert').strict;
 
-describe("integration test", function() {
-    it("should be able to complete the attributes", function() {
-        let  resultBuilder = new ResultBuilder();
+describe("integration test", function () {
 
+    let resultBuilder = new ResultBuilder();
+
+    it("should be able to complete the attributes", function () {
         //complete the elements of the 1st test
-        let  test1 = resultBuilder.newTest("unique nqme 1")
+        let test1 = resultBuilder.newTest("h1-heading")
         test1.setDescription("description 1")
-        test1.setTitle("title 1")
-        test1.setWeight(1)
-        test1.setScore(1)
-        test1.addRecommandation(1)
-        test1.addSnippet(" snippets 1")
+            .setTitle("H1 headings")
+            .setWeight(1)
+            .setScore(1)
+            .addRecommandation("zzzz")
+            .addSnippet(" snippets 1")
+            .addTable( [
+                "",
+                "URL",
+                "Resource Size",
+                "Potential Savings"
+              ])
+
         //complete the elements of the 2nd test
         let test2 = resultBuilder.newTest("unique name 2")
-        test2.setDescription("desc 2")
-        test2.setTitle("title 2")
-        test2.setWeight(1)
-        test2.setScore(1)
-        test2.addSnippet("snippet 2")
-        test2.addRecommandation("recommandation 2")
-        
-        if (resultBuilder.getResultsTests().length == 0) {
-            console.log("no data found !!!!!")
-        } else {
-            console.log(resultBuilder.getResultsTests().length)
-            console.log("---------------------Display withn toArray method ---------------------")
-            console.log(resultBuilder.toArray())
-            console.log("---------------------Display withn toJson method ---------------------")
-            console.log( resultBuilder.toJson());
-        }
+            .setDescription("desc 2")
+            .setTitle("title 2")
+            .setWeight(1)
+            .setScore(1)
+            .addSnippet("snippet 2")
+            .addRecommandation("recommandation 2")
 
-       
+    });
+
+    it('Check there is a test', function () {
+        assert.notEqual(resultBuilder.getResultsTests().length, 0, " Data not found !")
+        //assert.ok(resultBuilder.toJson())
     });
 });
