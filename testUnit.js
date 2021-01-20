a11f6b7e04edc9dbb0a00e70691627ff4a8b89d1
ppalioune <ppalioune@gmail.com>
2021-01-20 16:10:54 -0500

    upadate testUnit 20-01

--- testUnit.js
+++ testUnit.js
@@ -1,12 +1,16 @@
 const ResultBuilder = require('./resultBuilder')
-const Test = require('./test')
 const assert = require('assert').strict;
 
-describe("integration test", function () {
+describe("Building result", function () {
 
     let resultBuilder = new ResultBuilder();
 
-    it("should be able to complete the attributes", function () {
+    it('should start empty', function () {
+        assert.equal(resultBuilder.getResultsTests().length, 0)
+        //assert.ok(resultBuilder.toJson())
+    });
+
+    it("should be able to add a new entry", function () {
         //complete the elements of the 1st test
         let test1 = resultBuilder.newTest("h1-heading")
         test1.setDescription("description 1")
@@ -21,20 +25,48 @@ describe("integration test", function () {
                 "Resource Size",
                 "Potential Savings"
               ])
-
-        //complete the elements of the 2nd test
-        let test2 = resultBuilder.newTest("unique name 2")
-            .setDescription("desc 2")
-            .setTitle("title 2")
-            .setWeight(1)
-            .setScore(1)
-            .addSnippet("snippet 2")
-            .addRecommandation("recommandation 2")
-
+              assert.equal(resultBuilder.getResultsTests().length, 1)
     });
 
-    it('should test if there is a test', function () {
-        assert.notEqual(resultBuilder.getResultsTests().length, 0, " Data not found !")
-        //assert.ok(resultBuilder.toJson())
+    it("should keep test references internally", function() {
+        resultBuilder.newTest("name-1")
+        resultBuilder.newTest("name-2")
+        assert.equal(resultBuilder.getResultsTests().length, 3);
     });
+    it("should throw an error when a test already exists", function() {
+        resultBuilder.newTest("name-1");
+        assert.throws(() => { resultBuilder.newTest("name-1"); }, Error);
+    })
+    it("should test that the results are in json format", function () {
+        resultBuilder.newTest("name-4")
+        assert.ok(resultBuilder.isJson(resultBuilder.toJson()))
+    })
+
+    it("should test that the results are in array format ", function () {
+        resultBuilder.newTest("name-5")
+        assert.ok(Array.isArray(resultBuilder.getResultsTests()))
+    })
+   
+
+    it("Should throw an error when weight have invalid value, ", function () {
+        let test = resultBuilder.newTest("name-7")
+        test.setWeight(9)
+        assert.throws(() => { test.setScore("a string"); }, Error)
+    })
+
+    it("Should throw an error when score have invalid value, ", function () {
+        let test = resultBuilder.newTest("name-6")
+        test.setScore("a string")
+        assert.throws(() => { test.setScore("a string"); }, Error)
+    })
+    
+    /*it("Should be detect error when there is a valid value", function () {
+        resultBuilder.newTest("name-3")
+            .setTitle("title 3")
+            .setWeight(4)
+            .setScore(" score-3")
+            .setDescription(3)        
+        
+        console.log(resultBuilder.getResultsTests());
+    })*/
 });
